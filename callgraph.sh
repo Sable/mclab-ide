592b5f24e9ee9ca412827843cadcff36c25e330f
Ismail Badawi <ismail@badawi.io>
2014-03-04 16:10:33 -0500

    Log to temp file and dump to stdout

--- callgraph.sh
+++ callgraph.sh
@@ -2,8 +2,7 @@
 set -e
 
 USAGE="
-usage: callgraph.sh --project-dir=dir --entry-point=expr
-                    [--log-file=file] [--target-dir=dir]
+usage: callgraph.sh --project-dir=dir --entry-point=expr [--target-dir=dir]
 
 --project-dir DIR      Top level project directory to instrument.
                        Required.
@@ -11,8 +10,6 @@ usage: callgraph.sh --project-dir=dir --entry-point=expr
                        Required.
 --target-dir DIR       Directory in which to place instrumented code.
                        Defaults to '\$project-dir_instrumented'
---log-file FILE        File to which function calls / enters are logged.
-                       Defaults to 'trace.txt'
 "
 
 function main {
@@ -20,7 +17,7 @@ function main {
 
   project_dir=$1
   target_dir=$2
-  log_file="$current_dir/$3"
+  log_file=$3
   entry_point=$4
 
   natlabjar=$HOME/code/java/mclab/languages/Natlab/Natlab.jar
@@ -29,6 +26,7 @@ function main {
   cd $target_dir
   matlab >/dev/null -nojvm -r "mclab_callgraph_init('$log_file'); $entry_point; exit"
   cd $current_dir
+  cat $log_file
 }
 
 
@@ -39,7 +37,6 @@ function getarg {
 
 project_dir=''
 target_dir=''
-log_file=''
 entry_point=''
 
 while test $# != 0; do
@@ -49,9 +46,6 @@ while test $# != 0; do
     ;;
   --target-dir*)
     target_dir=$(getarg "$1")
-    ;;
-  --log-file*)
-    log_file=$(getarg "$1")
     ;;
   --entry-point*)
     entry_point=$(getarg "$1")
@@ -73,5 +67,7 @@ fi
 if [ -z "$target_dir" ]; then
   target_dir="$project_dir""_instrumented"
 fi
+
+log_file=$(mktemp -t "$0")
 
 main "$project_dir" "$target_dir" "$log_file" "$entry_point"
