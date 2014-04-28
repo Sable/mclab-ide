5a9aa4ebba9600c3df20f14ebc1e52c2f0a78ef9
Ismail Badawi <ismail@badawi.io>
2014-04-28 03:52:52 -0400

    Simplify tab view model.
    * Have the view model keep track of the selected tab instead of having
    a flag on each tab. This is nice since we don't need to loop through
    the tabs to find the selected one, and we don't need to unselect any
    other tabs when selecting one.
    * Use knockout subscriptions to decide when to trigger events, rather
    than just sticking the trigger call in the right place.

--- ide/static/js/tabs.js
+++ ide/static/js/tabs.js
@@ -2,7 +2,6 @@ ide.tabs = (function() {
   var Tab = function (name) {
     this.name = ko.observable(name);
     this.dirty = ko.observable(false);
-    this.selected = ko.observable(false);
   };
 
   var TabsViewModel = function (editor) {
@@ -11,36 +10,33 @@ ide.tabs = (function() {
     // TODO(isbadawi): Does this belong here?
     $(window).on('beforeunload', function() {
       // TODO(isbadawi): Prompt to save, not just warn?
-      var num_dirty = _(self.tabs()).filter(function (tab) {
-        return tab.dirty();
-      }).length;
-      if (num_dirty === 0) {
-        return;
+      var num_dirty = self.numDirtyTabs();
+      if (num_dirty > 0) {
+        var plural = num_dirty === 1 ? '' : 's';
+        return [
+          'You have ' + num_dirty + ' unsaved file' + plural + '.',
+          'If you leave the page, you will lose any unsaved changes.'
+        ].join('\n');
       }
-      var plural = num_dirty === 1 ? '' : 's';
-      return [
-        'You have ' + num_dirty + ' unsaved file' + plural + '.',
-        'If you leave the page, you will lose any unsaved changes.'
-      ].join('\n');
     });
 
-    self.tabs = ko.observableArray([]);
-    self.callbacks = {};
+    self.tabs = ko.observableArray();
+    self.selectedTab = ko.observable();
 
-    self.selectedTab = function() {
-      return _(self.tabs()).find(function (tab) {
-        return tab.selected();
-      });
-    };
-
-    self.selectTab = function(tab) {
-      self.tabs().forEach(function (tab) {
-        if (tab.selected()) {
-          tab.selected(false);
-        }
-      });
-      tab.selected(true);
+    self.selectedTab.subscribe(function (tab) {
       self.trigger('tab_select', tab.name());
+    });
+
+    self.tabs.subscribe(function (tabs) {
+      if (tabs.length === 0) {
+        self.trigger('all_tabs_closed');
+      }
+    });
+
+    self.numDirtyTabs = function() {
+      return _(self.tabs()).filter(function(tab) {
+        return tab.dirty();
+      }).length;
     };
 
     self.closeTab = function(tab) {
@@ -56,18 +52,14 @@ ide.tabs = (function() {
     };
 
     self.forceCloseTab = function(tab) {
-      if (tab.selected()) {
+      if (tab === self.selectedTab()) {
         var i = self.tabs.indexOf(tab);
         var next = self.tabs()[i == 0 ? 1 : i - 1];
         if (next) {
-          self.selectTab(next);
+          self.selectedTab(next);
         }
       }
       self.tabs.remove(tab);
-
-      if (self.tabs().length === 0) {
-        self.trigger('all_tabs_closed');
-      }
     };
 
     self.findByName = function(name) {
@@ -82,15 +74,17 @@ ide.tabs = (function() {
         tab = new Tab(path);
         self.tabs.push(tab);
       }
-      self.selectTab(tab);
+      self.selectedTab(tab);
     };
 
     self.on = function(event, callback) {
+      self.callbacks = self.callbacks || {};
       self.callbacks[event] = callback;
       return self;
     }
 
     self.trigger = function(event) {
+      self.callbacks = self.callbacks || {};
       var callback = self.callbacks[event];
       if (callback) {
         callback.apply(null, _(arguments).toArray().slice(1));
