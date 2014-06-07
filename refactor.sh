7a11c0418764d3be521ac80311adf6aafc5a42b7
Ismail Badawi <ismail@badawi.io>
2014-06-06 21:42:16 -0400

    Consolidate refactoring scripts.

--- refactor.sh
+++ refactor.sh
@@ -2,27 +2,19 @@
 set -e
 
 USAGE="
-usage: extract-variable.sh path selection newname
+usage: refactor.sh which path selection [args...]
 
+which        Refactoring to perform, e.g. ExtractVariable
 path         Path to file to refactor (e.g. myproject/f.m).
 selection    Range of text to apply refactoring to.
              Format: startline,startcolumn-endline,endcolumn, e.g. 2,1-4,5
-newname      Name of the new variable
+args         Any extra required params (e.g. newName for extract function)
 "
 
 function main {
-  path=$1
-  selection=$2
-  newname=$3
-
   natlabjar=$HOME/code/java/mclab/languages/Natlab/Natlab.jar
   refactoringbin=$HOME/code/java/mclab-ide-support/build
-  java -cp $natlabjar:$refactoringbin mclab.ide.refactoring.ExtractVariableTool $path $selection $newname
+  java -cp $natlabjar:$refactoringbin "mclab.ide.refactoring.$1Tool" "${@:2}"
 }
 
-if [ "$#" -ne "3" ]; then
-  echo "$USAGE"
-  exit 1
-fi
-
-main "$1" "$2" "$3"
+main "$@"
