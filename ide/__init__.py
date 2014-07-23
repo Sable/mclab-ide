59f96db27cf15e55a58b499fdc72746fcc2dc8d2
Ismail Badawi <ismail@badawi.io>
2014-07-23 19:44:03 -0400

    Add dummy secret key.

--- ide/__init__.py
+++ ide/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.jasmine import Jasmine, Asset
 from ide.assets import assets
 
 app = Flask(__name__)
-app.config.from_object('secret')
+app.config.from_object('ide.secret')
 app.config['ASSETS_DEBUG'] = True
 app.debug = True
 assets.init_app(app)
---
+++ ide/secret.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'sy)1!u5*$d=ou#t1#&e9^4el#)!)@9*3t62!8kzu0m_=clqqix'
